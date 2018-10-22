3c3d4e47739b1c2317d81d423f5b3262eeb9e3ec
Colton C <colto42@gmail.com>
2018-10-21 20:07:25 -0400

    Initial Commit

---
+++ Challenge 367 Easy Subfactorials/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_367_Easy_Subfactorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
